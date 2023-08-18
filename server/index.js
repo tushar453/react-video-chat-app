14197f05c86cca22c161dfe4c9dfb836dbbed7a9
SharmaTushar <sharmahimesh282@gmail.com>
2023-08-18 19:35:42 +0530

    aded

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const { Server } = require("socket.io");
-
-const io = new Server(8000, {
-  cors: true,
-});
-
+require("dotenv").config();
+const port = process.env.PORT || 8000;
+const io = new Server(port, { cors: true,});
+// { cors: true,}
+// 
 const emailToSocketIdMap = new Map();
 const socketidToEmailMap = new Map();
 
