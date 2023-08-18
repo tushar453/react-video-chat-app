318b659f1d7704e1b31d729d389ca541aa33a237
SharmaTushar <sharmahimesh282@gmail.com>
2023-08-18 16:30:35 +0530

    added

--- client/src/screens/Lobby.jsx
+++ client/src/screens/Lobby.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSocket } from "../context/SocketProvider";
-
+// import img from '../img/bird.png.png';
+import img from '../img/birds.png';
+import dog from '../img/png5.png';
+import { UilUser ,UilDialpadAlt } from '@iconscout/react-unicons'
 const LobbyScreen = () => {
   const [email, setEmail] = useState("");
   const [room, setRoom] = useState("");
@@ -33,27 +36,47 @@ const LobbyScreen = () => {
   }, [socket, handleJoinRoom]);
 
   return (
-    <div>
-      <h1>Lobby</h1>
+    <div className="Lobby" >
+      <div className="left" >
+        <img src={dog} alt="" className="img"/>
+      </div>
+      {/* <img src={img} alt="" /> */}
+      <div className="right">
+      <h1 className="h2">Welcome !</h1>
+      <h1 className="h1">Sign In</h1>
       <form onSubmit={handleSubmitForm}>
-        <label htmlFor="email">Email ID</label>
+        {/* <label htmlFor="email" className="label">Email ID</label> */}
+        <div className="input-container">
+        <span class="icon"><UilUser className="icons"/></span>
         <input
+
+className="transparent-input"
           type="email"
           id="email"
+          placeholder="Enter Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        </div>
         <br />
-        <label htmlFor="room">Room Number</label>
+        <div className="input-container">
+
+       
+        <span class="icon"> < UilDialpadAlt  className="icons"/></span>
+       
+        {/* <label className="label" htmlFor="room">Room Number</label> */}
         <input
           type="text"
           id="room"
+          className="transparent-input"
+          placeholder="Enter Room No."
           value={room}
           onChange={(e) => setRoom(e.target.value)}
-        />
+        /> </div>
         <br />
-        <button>Join</button>
+        <button className="button">Join</button>
       </form>
+      </div>
     </div>
   );
 };
--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useCallback, useState } from "react";
 import ReactPlayer from "react-player";
 import peer from "../service/peer";
 import { useSocket } from "../context/SocketProvider";
+import './Room.css';
 
 const RoomPage = () => {
   const socket = useSocket();
@@ -110,35 +111,50 @@ const RoomPage = () => {
   ]);
 
   return (
-    <div>
-      <h1>Room Page</h1>
-      <h4>{remoteSocketId ? "Connected" : "No one in room"}</h4>
-      {myStream && <button onClick={sendStreams}>Send Stream</button>}
-      {remoteSocketId && <button onClick={handleCallUser}>CALL</button>}
-      {myStream && (
-        <>
-          <h1>My Stream</h1>
-          <ReactPlayer
-            playing
-            muted
-            height="100px"
-            width="200px"
-            url={myStream}
-          />
-        </>
-      )}
+    <div className="Room">
+      <h1 className="room-h1">Room Page</h1>
+      <h4 className="room-h4">{remoteSocketId ? "Connected !" : "No one in room"}</h4>
+      {myStream && <button onClick={sendStreams} className="buttons">Send Stream</button>}
+      {remoteSocketId && <button onClick={handleCallUser} className="buttons">CALL</button>}
       {remoteStream && (
         <>
-          <h1>Remote Stream</h1>
+          <h1 className="room-h">Remote Stream</h1>
+          <div className="remote-player">
           <ReactPlayer
             playing
             muted
-            height="100px"
-            width="200px"
+            height="100%"
+            width="100%"
             url={remoteStream}
+          /></div>
+        </>
+        
+      )}
+
+
+
+
+
+      {myStream && (
+       
+        <>
+          {/* <h1 className="room-h">My Stream</h1> */}
+          <div className="player" >
+             <ReactPlayer
+            playing
+            muted
+            height="100%"
+            width="100%"
+            
+            url={myStream}
+            
           />
+          </div>
+         
         </>
       )}
+   
+      
     </div>
   );
 };
